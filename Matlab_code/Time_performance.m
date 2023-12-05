eeeba1283bfd42c0f89c40d7a0044df7170a8bc1
Giosue Cavagna <giosue.cavagna@outlook.it>
2023-12-05 15:51:48 +0100

    update

--- Matlab_code/Time_performance.m
+++ Matlab_code/Time_performance.m
@@ -1,12 +1,12 @@
 clear all
 n_MC=5;
-n_CoP=50;
+n_CoP=15;
 periods=200;
 tic
 [c_e1,c_e2,q]=Data_simulation(n_MC,n_CoP,periods);
 toc
 
-%time=1.210449
-%Elapsed time is 10,2, 193 20.493271seconds.
-%Elapsed time is 500,500 200197343.640984
+%time_for_simulation =1.210449
+%total time=~ time for simulation *n_MC*n_CoP
+%Elapsed time is 500,500 200 -> 197343.640984
 %(1.210449*(500*500))/(3600*24)=~3,5 days
